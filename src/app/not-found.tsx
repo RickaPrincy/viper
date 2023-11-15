37ad4af804eb48a0e2782b1c786215f9dde11f29
RickaPrincy <rckprincy@gmail.com>
2023-11-15 23:06:29 +0300

    feat: implement not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+function PageNotFound(){
+    return (
+        <div className="w-screen text-white h-screen text-[40px] flex-col flex items-center justify-center">
+            <h2 className="text-inherit">Page not found</h2>
+            <h3 className="text-inherit">404</h3>
+        </div>
+    )
+}
+
+export default PageNotFound
